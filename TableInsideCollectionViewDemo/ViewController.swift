e145cd3be637d46fc9167b8084ca218e8f7c3dc3
Amit Kumar Singh <amit.aanmol@gmail.com>
2019-01-23 07:36:55 +0530

    Initial Commit

---
+++ TableInsideCollectionViewDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableInsideCollectionViewDemo
+//
+//  Created by Amit Kumar Singh on 23/01/19.
+//  Copyright © 2019 Amit Kumar Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
